5c64c536cd25162e5d8db0b72e6ad1ef88ed8bb8
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-14 16:38:33 +0200

    added 2 var with 2 array with different lenght , added a for so we can cicle the if condition

---
+++ array_bonus/js/script.js
@@ -0,0 +1,11 @@
+
+const arrayFirst = [1 , 2 , 3 , 4 , 5 ];
+const arraySecond = [1 , 2 , 31 , 66 , 92 , 4 , 5  , 23 , 22 , 11 ,5456 ,1231 ];
+
+for (let i = 0 ; i <arraySecond.length; i++  ){
+    
+    if(arrayFirst.length < arraySecond.length){
+        arrayFirst.push(i)
+        console.log(arrayFirst.length , arraySecond.length)
+} 
+} 
