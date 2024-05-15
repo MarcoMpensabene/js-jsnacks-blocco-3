caa6a48c6c567151443a7e6a9f35ca8b6882ae85
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-15 12:15:12 +0200

    added array and var

---
+++ jsSnack10/script.js
@@ -0,0 +1,7 @@
+// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+let arrayA = [1  , 2 , 3 , 4 ,5 ];
+let arrayB = [1  , 2 , 3 , 4 ,5 , 11 ,23 ,45 ,66 ,77 ];
+
+let arrayShorter;
+let arrayLonger;
