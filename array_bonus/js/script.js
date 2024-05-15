b64094bdf34dc03ede2c175a725919db23f669a7
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-15 10:35:54 +0200

    added 2 variables for the array so we know which array should be pushed with the cicle for

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -2,10 +2,22 @@
 const arrayFirst = [1 , 2 , 3 , 4 , 5 ];
 const arraySecond = [1 , 2 , 31 , 66 , 92 , 4 , 5  , 23 , 22 , 11 ,5456 ,1231 ];
 
-for (let i = 0 ; i <arraySecond.length; i++  ){
-    
-    if(arrayFirst.length < arraySecond.length){
-        arrayFirst.push(i)
-        console.log(arrayFirst.length , arraySecond.length)
+let arrayShort;
+let arrayLong;
+
+if (arrayFirst.length === arraySecond.length){
+    console.log("Stessa lunghezza");
+} else if (arrayFirst.length > arraySecond.length){
+    arrayLong= arrayFirst;
+    arrayShort = arraySecond;
+} else {
+    arrayShort = arrayFirst;
+    arrayLong = arraySecond;
+}   
+
+const elements = arrayLong.length  - arrayShort.length;
+for (let i = 0 ; i <elements; i++  ){
+        arrayShort.push(i);
 } 
-} 
+
+console.log(arrayLong , arrayShort , arrayLong.length , arrayShort.length);
