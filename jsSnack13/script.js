d68b7d5112d8104f2816b2719b405f44627c4ba7
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-16 12:20:06 +0200

    added a function with a while for obtaining the last cipher of the number and cut if

---
+++ jsSnack13/script.js
@@ -0,0 +1,23 @@
+//  > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
+
+let userNumber = Number.parseInt(prompt("type a number") , 10);
+
+function numberInverter(num , reverse){
+// Inizializziamo al variabile
+reverse = 0;
+
+while(num>0){
+    // Ottieni la crifra più esterna da destra 
+    rightmost = num%10;
+    
+    reverse = reverse*10 + rightmost;
+    
+    // Rimuovi la cifra più esterna da destra 
+    num = Math.floor(num/10);
+}
+return reverse;
+}
+
+let result = numberInverter(userNumber);
+
+console.log(result)
