66ab930aa7b87176db35391439de9c59bf7020b1
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-21 12:36:52 +0200

    added element to the DOM

--- jsSnack14/script.js
+++ jsSnack14/script.js
@@ -1,7 +1,7 @@
 // ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
 let userSecond = Number.parseInt(prompt("Inserisci i secondi") , 10);
-
+let divEl = document.querySelector("div")
 const timer = setInterval(myTime , 1000);
 
 function myTime() {
@@ -10,6 +10,10 @@ if (userSecond > 10 || isNaN(userSecond)){
     userSecond = 9;
 }
 userSecond--;
+let pEl = document.createElement("p")
+divEl.appendChild(pEl);
+pEl.innerText = userSecond;
+
 console.log(userSecond)
 if (userSecond <= 0){
     clearInterval(timer)
