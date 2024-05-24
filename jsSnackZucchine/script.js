958fe22ff2bf80e44bd45cd38132a4f60abd386b
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-24 11:26:58 +0200

    added array of obj , filtered the array in two array

---
+++ jsSnackZucchine/script.js
@@ -0,0 +1,34 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+// Dividi in due array separati le zucchine che misurano meno o più di 8cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+
+const zucchine = [
+    {varieta : "varietàA" , peso : 100 , lunghezza : 5 },
+    {varieta : "varietàB" , peso : 200 , lunghezza : 9 },
+    {varieta : "varietàC" , peso : 300 , lunghezza : 13 },
+    {varieta : "varietàD" , peso : 56 , lunghezza : 3 },
+    {varieta : "varietàA" , peso : 120 , lunghezza : 7 },
+    {varieta : "varietàD" , peso : 140 , lunghezza : 8 },
+    {varieta : "varietàF" , peso : 160 , lunghezza : 9 },
+    {varieta : "varietàB" , peso : 240 , lunghezza : 11 },
+    {varieta : "varietàD" , peso : 302 , lunghezza : 14 },
+    {varieta : "varietàE" , peso : 350 , lunghezza : 15 },
+]
+
+const longZucchine = zucchine.filter(zucchineL => {
+    if (zucchineL.lunghezza > 8){
+        return zucchineL
+    } 
+})
+
+console.log(longZucchine)
+
+const smallZucchine = zucchine.filter(zucchineL => {
+    if (zucchineL.lunghezza < 8){
+        return zucchineL
+    } 
+})
+
+console.log(smallZucchine)
